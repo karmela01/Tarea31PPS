19079b76bbbcf4c1839e5f6f6a5a9a6de9a4e44f
karmela01 <carmencastro0101@gmail.com>
2022-04-17 14:40:01 +0000

    quinto commit: Modificanado Jenkinsfile (añadiendo etapa análisis estático)

--- src/main/java/soy/CARMENCASTRO/sumador/SumadorApplication.java
+++ src/main/java/soy/CARMENCASTRO/sumador/SumadorApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SumadorApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SumadorApplication.class, args);
-	}
-
+		}
 }
